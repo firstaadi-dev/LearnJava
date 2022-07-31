823916368443c29ea4e1a24ddcb7e9616d4dcd30
Firsta Adi Pradana <firstaadi@hotmail.com>
2022-07-31 15:34:26 +0700

    chapter 32

--- src/Primitive/CharBoolean.java
+++ src/Primitive/CharBoolean.java
@@ -1,4 +1,20 @@
 package Primitive;
 
+import java.io.IOException;
+
 public class CharBoolean {
+    public static void main(String[] args) throws IOException {
+        char mySpace = '\u0C18';
+        System.out.println(mySpace);
+
+        boolean isLegal;
+
+        int customerAge = System.in.read();
+
+        isLegal = customerAge > 18;
+
+        System.out.println(isLegal);
+
+    }
+
 }
